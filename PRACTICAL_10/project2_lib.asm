49156a2b36f84f8958881b5db3c297d6ec1c329e
rohen-13 <231649092+rohen-13@users.noreply.github.com>
2026-04-24 18:09:13 +0300

    add register-based addition routine

--- PRACTICAL_10/project2_lib.asm
+++ PRACTICAL_10/project2_lib.asm
@@ -1,2 +1,11 @@
 ; Project 2 helper routines
 ; Register and stack parameter passing will go here
+
+global register_adder
+
+section .text
+
+register_adder:
+    mov eax, edi
+    add eax, esi
+    ret
