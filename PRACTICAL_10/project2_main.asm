cc1430b1480e86cceb9b8e16f48860cd04ce56e2
rohen-13 <231649092+rohen-13@users.noreply.github.com>
2026-04-27 17:27:19 +0300

    read first integer input

--- PRACTICAL_10/project2_main.asm
+++ PRACTICAL_10/project2_main.asm
@@ -3,15 +3,23 @@
 
 global main
 extern printf
+extern scanf
 
 section .data
     prompt db "Enter number: ", 0
     result db "The sum is: ", 0
     final_result db "Final sum is: ", 0
+    input_format db "%d", 0
+
+section .bss
+    first_number resd 1
 
 section .text
 
 main:
+    push rbp
+    mov rbp, rsp
+
     mov ebx, 0
     mov ecx, 3
 
@@ -20,7 +28,13 @@ game_loop:
     mov eax, 0
     call printf
 
+    lea rdi, [input_format]
+    lea rsi, [first_number]
     mov eax, 0
+    call scanf
+
+    mov eax, 0
+    pop rbp
     ret
 
 section .note.GNU-stack noalloc noexec nowrite progbits
